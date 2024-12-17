ede74fb13d33ebfc65eacc8be3e70c29856ee20e
“jaimesnh” <jaimesv1203@gmail.com>
2024-12-17 18:38:49 +0100

    añadida clase Manu

--- src/products/Menu.java
+++ src/products/Menu.java
@@ -4,10 +4,47 @@
  */
 package products;
 
+import java.util.*;
+
 /**
  *
  * @author jaime
  */
-public class Menu {
+public class Menu implements Product{
     
+    private List<IndividualProduct> products;
+    private static int discount;  
+    
+    @Override
+    public int getPrice() {
+        int total = 0;
+
+        for (IndividualProduct product : products) {
+            total += product.getPrice();
+        }
+
+        total -= (total * discount) / 100;
+        return total;
+    }
+
+    @Override
+    public String getName() {
+
+        StringBuilder nameBuilder = new StringBuilder("Menu compuesto por: ");
+        for (IndividualProduct product : products) {
+            nameBuilder.append(product.getName()).append(", ");
+        }
+
+        return nameBuilder.substring(0, nameBuilder.length() - 2);
+    }
+    
+    public Product getProduct(int i) {
+        return products.get(i);
+    }
+    
+    public int getNumProducts() {
+        return products.size();
+    }
+    
+    // FALTA EL LEER Y ESCRIBIR DE ARCHIVO 
 }
