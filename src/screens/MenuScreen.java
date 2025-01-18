cd9893488f0601b7578a898aae8a17ea483d9a1b
“jaimesnh” <jaimesv1203@gmail.com>
2025-01-18 16:11:50 +0100

    cccx

--- src/screens/MenuScreen.java
+++ src/screens/MenuScreen.java
@@ -4,9 +4,13 @@
  */
 package screens;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import manager.Context;
 import manager.SimpleKiosk;
 import products.IndividualProduct;
+import products.Menu;
 import products.MenuCard;
 import products.MenuCardSection;
 import products.Order;
@@ -22,47 +26,55 @@ public class MenuScreen extends CarouselScreen {
     
         @Override
     public KioskScreen show(Context c) {
-        SimpleKiosk kiosk = c.getKiosk();
-        MenuCard mc = c.getMenuCard();
-        MenuCardSection mcs = mc.getSection(currentSection);
-        IndividualProduct p = mcs.getProduct(currentProduct);
-        int numProducts = mcs.getNumberOfProducts();
-        
-               
-        kiosk.clearScreen();
-        configureScreenButtons(kiosk, mcs, p);
-        super.adjustCarruselButtons(kiosk, currentProduct, numProducts-1);
-        
-        char event = kiosk.waitEvent(30);
-           
-        switch (event) {
-            case 'A': // Añadir
-                if (currentSection < mc.getNumberOfSections()-1) {
-                    Order o = c.getOrder();
-                    o.addProduct(p);
-                    currentSection ++;
-                    return this;                  
-                } else {               
-                return new OrderScreen();
-                }
-            case 'B': // Cancelar menú
-                return new OrderScreen();
-            case 'C': // Cancelar pedido
-                return new WelcomeScreen();
-            case 'G': // Anterior 
-                if (currentProduct > 1) {
-                    currentProduct--;
-                }
-                return this;
-            case 'H': // Siguiente 
-                if (currentProduct < numProducts) {
-                    currentProduct++;
-                }
-                return this;
-            default:
-                return this;
+        try {
+            SimpleKiosk kiosk = c.getKiosk();
+            MenuCard mc = c.getMenuCard();
+            MenuCardSection mcs = mc.getSection(currentSection);
+            IndividualProduct p = mcs.getProduct(currentProduct);
+            Menu m = new Menu();
+            System.out.println(m.discount);
+            int numProducts = mcs.getNumberOfProducts();
+            
+            
+            kiosk.clearScreen();
+            configureScreenButtons(kiosk, mcs, p);
+            super.adjustCarruselButtons(kiosk, currentProduct, numProducts-1);
+            
+            char event = kiosk.waitEvent(30);
+            
+            switch (event) {
+                case 'A': // Añadir
+                    if (currentSection < mc.getNumberOfSections()-1) {
+                        m.addProductToMenu(p);
+                        currentSection ++;
+                        return this;
+                    } else {
+                        m.addProductToMenu(p);
+                        Order o = c.getOrder();
+                        o.addProduct(m);
+                        return new OrderScreen();
+                    }
+                case 'B': // Cancelar menú
+                    return new OrderScreen();
+                case 'C': // Cancelar pedido
+                    return new WelcomeScreen();
+                case 'G': // Anterior
+                    if (currentProduct > 1) {
+                        currentProduct--;
+                    }
+                    return this;
+                case 'H': // Siguiente
+                    if (currentProduct < numProducts) {
+                        currentProduct++;
+                    }
+                    return this;
+                default:
+                    return this;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MenuScreen.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        return this;
     }
     
     public void configureScreenButtons(SimpleKiosk kiosk, MenuCardSection mcs, IndividualProduct p) {
